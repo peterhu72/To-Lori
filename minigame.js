ba4e6d62038dbe846e7835549ba1bd2b06dd56ba
peterhu72 <peterhu4000@gmail.com>
2025-08-26 17:04:40 -0600

    second

---
+++ minigame.js
@@ -0,0 +1,37 @@
+function onClick() {
+    console.log('hi')
+    let lori = document.getElementById("SOUZA")
+    if (lori.style.display === 'none') {
+        lori.style.display = 'block'
+    } else {
+        lori.style.display = 'none'        
+    }
+}
+
+function onClick1() {
+    console.log('hi')
+    let lori = document.getElementById("SOUZA1")
+    if (lori.style.display === 'none') {
+        lori.style.display = 'block'
+    } else {
+        lori.style.display = 'none'        
+    }
+}
+function onClick2() {
+    console.log('hi')
+    let lori = document.getElementById("SOUZA2")
+    if (lori.style.display === 'none') {
+        lori.style.display = 'block'
+    } else {
+        lori.style.display = 'none'        
+    }
+}
+function onClick3() {
+    console.log('hi')
+    let lori = document.getElementById("SOUZA3")
+    if (lori.style.display === 'none') {
+        lori.style.display = 'block'
+    } else {
+        lori.style.display = 'none'        
+    }
+}
